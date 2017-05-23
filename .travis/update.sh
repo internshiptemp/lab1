3b534e5f165495572a8ba6e4823c09187cf757e4
stuarthoye <stuarthoye@gmail.com>
2017-05-23 12:44:03 -0600

    New auth attempt.

--- .travis/update.sh
+++ .travis/update.sh
@@ -8,11 +8,21 @@ echo "!!!"
 echo "$GIT_TOKEN"
 echo "!!!"
 
+AUTH_ID="stuarthoye"
 git config --global user.email "stuarthoye@gmail.com"
 git config --global user.name "stuarthoye"
 git config --global github.user "stuarthoye"
 git config --global github.token $GIT_TOKEN
+
+URL=$(git remote get-url origin)
+NEWURL="${URL[@]:0:8}$AUTH_ID:$GIT_TOKEN@${URL[@]:8}"
+echo $NEWURL
+
+git config remote.origin.url $NEWURL
+
 git add ./.travis/diagnostics/
 git commit -m "Responding with test case results."
 git config --global push.default matching
 git push
+
+git config remote.origin.url $URL
