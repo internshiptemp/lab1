f33c1b6f878038ddc2519db2e9eff810bffdb708
stuarthoye <stuarthoye@gmail.com>
2017-05-23 11:38:15 -0600

    Try again.

--- .travis/update.sh
+++ .travis/update.sh
@@ -8,7 +8,7 @@ git config --global user.email "stuarthoye@gmail.com"
 git config --global user.name "stuarthoye"
 git config --global github.user "stuarthoye"
 git config --global github.token $GIT_TOKEN
-git add ./travis/diagnostics/
+git add ./.travis/diagnostics/
 git commit -m "Responding with test case results."
 git config --global push.default matching
 git push
