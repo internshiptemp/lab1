26263ff45b262f424aaef44a2bdeccf834bbb663
stuarthoye <stuarthoye@gmail.com>
2017-05-23 11:04:05 -0600

    Try to set up Travis push w/CI info.

--- .travis/update.sh
+++ .travis/update.sh
@@ -4,6 +4,8 @@
 #       then we can use the push to notify students of their
 #       test-case status.
 
+git config --global github.user stuarthoye
+git config --global github.token $GIT_TOKEN
 git add ./travis/diagnostics/
 git commit -m "Responding with test case results."
 git config --global push.default matching
