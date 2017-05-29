839f04cf54ba236d19a3219596f2bf8fade5788c
stuarthoye <stuarthoye@gmail.com>
2017-05-29 10:44:21 -0600

    Add a passing file to test CI.

--- submission/lab0.s
+++ submission/lab0.s
@@ -26,7 +26,18 @@ main:
 	li	$v0, 5				# syscall readint
 	syscall					# $v0 <- 0xWWXXYYZZ
 
-	# Flip the integer here!
+	andi $t0, $v0, 0xFF     # $t0 <- 0x000000ZZ
+    sll  $a0, $t0, 8        # $a0 <- 0x0000ZZ00 
+    srl  $v0, $v0, 8        # $v0 <- 0x00WWXXYY
+    andi $t0, $v0, 0xFF     # $t0 <- 0x000000YY
+    or   $a0, $a0, $t0      # $v0 <- 0x0000ZZYY
+    sll  $a0, $a0, 8        # $a0 <- 0x00ZZYY00
+    srl  $v0, $v0, 8        # $v0 <- 0x0000WWXX
+    andi $t0, $v0, 0xFF     # $t0 <- 0x000000XX
+    or   $a0, $a0, $t0      # $a0 <- 0x00ZZYYXX
+    sll  $a0, $a0, 8        # $a0 <- 0xZZYYXX00
+    srl  $v0, $v0, 8        # $v0 <- 0x000000WW
+    or   $a0, $a0, $v0      # $a0 <- 0xZZYYXXWW
 
 	li	$v0, 1				# syscall printint
 	syscall
