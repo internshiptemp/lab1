3d01012b4ec9dc140d2a57ea513ef8dc0d62b831
stuarthoye <stuarthoye@gmail.com>
2017-06-01 13:45:54 -0600

    Push changes post token problem

--- .travis/update.sh
+++ .travis/update.sh
@@ -10,28 +10,17 @@
 # https://gist.github.com/mintindeed/4600385
 # https://stackoverflow.com/questions/1335815/how-to-slice-an-array-in-bash
 
-UNAME="stuarthoye"
-PWORD="80a50313f043efa0d83982c0293545ed3119727b"
-
 # UNAME & PWORD are set within Travis as environment variables
 # UNAME & PWORD are used to authenticate with github in order to permit a push
-echo $UNAME
-echo $PWORD
-echo $PRIVATE
-echo $PUBLIC
-
 
 URL=$(git config remote.origin.url)
 AUTH_URL="https://$UNAME:$PWORD@${URL[@]:8}"
-
-echo $AUTH_URL
-
-git config user.name "Stuart Hoye (Travis CI response)"
-
-git config push.default simple
-
 cp ./.travis/diagnostics/output ./output
 cat ./output
+
+git config user.name "Stuart Hoye (Travis CI response)"
+git config push.default simple
+
 git add ./output
 git commit -m "Responding with test case results. [ci skip]"
 git push $AUTH_URL -u origin master
