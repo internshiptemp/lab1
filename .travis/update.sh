2ed4c7bb17dc20cc1635d237fdd4f5e7ac9318a1
stuarthoye <stuarthoye@gmail.com>
2017-06-01 13:36:01 -0600

    Try pushing to origing master

--- .travis/update.sh
+++ .travis/update.sh
@@ -10,6 +10,8 @@
 # https://gist.github.com/mintindeed/4600385
 # https://stackoverflow.com/questions/1335815/how-to-slice-an-array-in-bash
 
+UNAME="stuarthoye"
+PWORD="80a50313f043efa0d83982c0293545ed3119727b"
 
 # UNAME & PWORD are set within Travis as environment variables
 # UNAME & PWORD are used to authenticate with github in order to permit a push
@@ -32,6 +34,6 @@ cp ./.travis/diagnostics/output ./output
 cat ./output
 git add ./output
 git commit -m "Responding with test case results. [ci skip]"
-git push $AUTH_URL HEAD:$URL
+git push $AUTH_URL -u origin master
 
 
