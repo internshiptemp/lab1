cbd21b1a7f8d63471f8e1f276b8ce5d8bd5a9fab
stuarthoye <stuarthoye@gmail.com>
2017-06-01 09:28:26 -0600

    Try fooling w/environ vars in Travis.

--- .travis/update.sh
+++ .travis/update.sh
@@ -4,25 +4,29 @@
 #       then we can use the push to notify students of their
 #       test-case status.
 
-git config -l
+# git config -l
 
-git config --global user.email "stuarthoye@gmail.com"
-git config --global user.name "stuarthoye"
-git config --global github.user "stuarthoye"
-git config --global github.password $GIT_TOKEN
+# git config --global user.email "stuarthoye@gmail.com"
+# git config --global user.name "stuarthoye"
+# git config --global github.user "stuarthoye"
+# git config --global github.password $GIT_TOKEN
 
-git config -l
+# git config -l
 
-git add ./.travis/diagnostics/
-git commit -m "Responding with test case results."
-git config --global push.default matching
+# git add ./.travis/diagnostics/
+# git commit -m "Responding with test case results."
+# git config --global push.default matching
 
 # https://gist.github.com/mintindeed/4600385
 # https://stackoverflow.com/questions/1335815/how-to-slice-an-array-in-bash
-UNAME="stuarthoye"
-PWORD=$GIT_TOKEN
-URL=$(git remote get-url --push origin)
-AUTH_URL="https://$UNAME:$PWORD@${URL[@]:8}"
-git push $AUTH_URL
+# UNAME="stuarthoye"
+# PWORD=$GIT_TOKEN
+# URL=$(git remote get-url --push origin)
+# AUTH_URL="https://$UNAME:$PWORD@${URL[@]:8}"
+# git push $AUTH_URL
 
 # git config remote.origin.url $URL
+
+
+echo $PRIVATE
+echo $PUBLIC
